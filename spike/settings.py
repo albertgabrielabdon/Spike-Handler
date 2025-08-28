f8e9f58bf456b4c57c6dba9d927dfbc8f84495a8
albertgabrielabdon <157718121+albertgabrielabdon@users.noreply.github.com>
2025-08-28 13:30:05 +0800

    another dumb mistake

--- spike/settings.py
+++ spike/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['albertabdon.pythonanywhere.com, '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['albertabdon.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
